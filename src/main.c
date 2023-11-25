3e84b475717c8ab631517526c4ca84615d4af09d
Hannes Erik Rother <hrother@c2r7p6.42vienna.com>
2023-11-25 02:36:57 +0100

    123

--- src/main.c
+++ src/main.c
@@ -1,17 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/25 01:37:08 by hrother           #+#    #+#             */
-/*   Updated: 2023/11/25 01:43:15 by hrother          ###   ########.fr       */
+/*   Created: 2023/11/25 01:42:17 by hrother           #+#    #+#             */
+/*   Updated: 2023/11/25 02:35:58 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include <mlx.h>
 
+int	main(int argc, char **argv)
+{
+	void	*mlx;
+	void	*mlx_win;
 
-#endif
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 720, 720, "Hello world!");
+	mlx_loop(mlx);
+	return (0);
+}
