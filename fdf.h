9c39dc0149af1941486cc562eadd2b0864cda336
Hannes Erik Rother <hrother@c2r6p3.42vienna.com>
2023-12-09 16:46:28 +0100

    changes

--- fdf.h
+++ fdf.h
@@ -6,12 +6,27 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 01:37:08 by hrother           #+#    #+#             */
-/*   Updated: 2023/11/25 01:43:15 by hrother          ###   ########.fr       */
+/*   Updated: 2023/12/09 16:40:44 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
+#include <mlx.h>
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,19 +6,54 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 01:42:17 by hrother           #+#    #+#             */
-/*   Updated: 2023/11/25 02:35:58 by hrother          ###   ########.fr       */
+/*   Updated: 2023/12/09 16:40:36 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "../fdf.h"
+
+void	put_pixel(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	close(int keycode, t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_vars vars;
+	t_data	img;
+	int		win_w = 1280;
+	int		win_h = 720;
+	int		y;
+	int		x;
+	int		anim_offset;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 720, 720, "Hello world!");
-	mlx_loop(mlx);
-	return (0);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, win_w, win_h, "Hello world!");
+	mlx_hook(vars.win, 17, 1L<<0, close, &vars);
+	img.img = mlx_new_image(vars.mlx, win_w, win_h);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	y = 0;
+	anim_offset = 0;
+	while (y < win_h)
+	{
+		x = 0;
+		while (x < win_w)
+		{
+			put_pixel(&img, x, y, 255*255*x + y + anim_offset /*+ 255 * anim_offset*/);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	anim_offset++;
+	mlx_loop(vars.mlx);
 }
